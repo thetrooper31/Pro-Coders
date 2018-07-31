2b3634edfe1bdad09465a625bc406d34432ecb43
Exequiel <exequiel_pina@hotmail.es>
2018-07-31 19:02:39 +0200

    (no había grabado cambios en visual, en comit anterior)

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function () {
+    "use strict";
+    var regalo = document.getElementById('regalo');
+    document.addEventListener('DOMContentLoaded', function () {
+      //Seleccionamos elemento por id       Campos Datos Usuario
+      var nombre = document.getElementById('nombre');
+      var apellido = document.getElementById('apellido');
+      var email = document.getElementById('email');
+      //Campos PAses
+      var pase_dia = document.getElementById('pase_dia');
+      var pase_dosdias = document.getElementById('pase_dosdias');
+      var pase_completo = document.getElementById('pase_completo');
+      //botones y divs
+      var calcular = document.getElementById('calcular');
+      var errordiv = document.getElementById('error');
+      var botonRegistro = document.getElementById('btnRegistro');
+      var lista_productos = document.getElementById('lista-productos')
+      var suma = document.getElementById('suma-total');
+     
+      
+      });
+    })(); 
